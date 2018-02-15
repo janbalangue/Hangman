d1ae0aa15d5b99026f40df4d786d487a25388e1b
Jan Balangue <jantonio.balangue@gmail.com>
2018-02-15 13:08:35 -0800

    Modified game.js. Almost all features working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
 // I know the number at element 0 of each array is worthless but the program will break if I remove it.
 var composers = [
     ["goodvideo", "Rachmaninoff", "Sergei", "1873-1943", "Piano Concerto No. 2 Op. 18, 2nd Movement", "Yuja Wang, Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/qQCH0QPoCfs?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=4" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Schubert", "Franz", "1797-1828", "Impromptu Op. 90 No. 3", "Alfred Brendel, Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/s15zyzSxrdI?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=6" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
+    ["goodvideo", "Schubert", "Franz", "1797-1828", "Impromptu Op. 90 No. 1", "Alfred Brendel, Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/zqqC0zbDkn4?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=12" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
     ["goodvideo", "Beethoven", "Ludwig Van", "1770-1827", "Sonata Pathetique, 2nd Movement", "Alfred Brendel, Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/HrbZDgsZUBw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
     ["goodvideo", "Holst", "Gustav", "1874-1934", "The Planets I. Mars, Bringer of War", "Royal Philharmonic Orchestra, Sir Charles Groves, Conductor", '<iframe width="560" height="315" src="https://www.youtube.com/embed/KUTByW8NElo?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
     ["goodvideo", "Mozart", "Wolfgang Amadeus", "1756-1791", "Piano Sonata No. 11 in A, First Movement", "Mitsuko Uchida, Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/qbn692EKQLw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=2" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
@@ -48,30 +48,34 @@ var alphabet = {
     "y": "unset",
     "z": "unset"
 };
-var numberOfGuesses;
-var randomComposerIndex;
-var puzzleAnswer;
-var answerSoFar;
+var numberOfGuesses = 0;
+var randomComposerIndex = 0;
+var puzzleAnswer = "";
+var answerSoFar = "";
 var wrongLetters = [];
-var wrongLetterDisplay;
-var correctLetters;
+var wrongLetterDisplay = "";
+var correctLetters = 0;
+var correctTimesInAnswer = 0;
 
 function initializeSettings() {
     numberOfGuesses = 15;
     // initialize each letter value to unselected
     displayNumberOfGuesses(numberOfGuesses);
+    correctLetters = 0;
 
     randomComposerIndex = Math.floor(Math.random() * composers.length);
     // assign random composer as the hangman answer
     puzzleAnswer = composers[randomComposerIndex][1].toUpperCase(); // makes answer uppercase; index [][1] is the composer's last name
     console.log("randomComposerIndex: " + randomComposerIndex);
+    console.log("puzzleAnswer: " + puzzleAnswer);
+
+
     for (var letter in alphabet) {
         alphabet[letter] = "unselected";
     }
     document.getElementById("win-counter").innerHTML = wins;
     document.getElementById("loss-counter").innerHTML = losses;
 
-    console.log("puzzleAnswer: " + puzzleAnswer);
 
     answerSoFar = "";
     // write correct number of spaces to HTML
@@ -84,7 +88,6 @@ function initializeSettings() {
     displayWrongLetters(wrongLetters);
     // if letter is unselected, check if letter is in puzzleAnswer
     // if letter is correct, output it in HTML document along with other correct letters as many times as needed
-    correctLetters = 0;
 }
 
 function displayNumberOfGuesses(number) {
@@ -97,17 +100,17 @@ function displayAnswerSoFar(answerSoFar) {
     console.log("answerSoFar: " + answerSoFar);
 }
 
-function displayWrongLetters(wrongLetters[]){
+function displayWrongLetters(wrongLetters) {
+    wrongLetterDisplay = "";
     if (wrongLetters.length > 0) {
-        wrongLetterDisplay = "";
         // if letter is wrong, output it in HTML document along with other wrong letters in order of entry
         for (var i = 0; i < wrongLetters.length; i++) {
             wrongLetterDisplay += wrongLetters[i] + " ";
             console.log("wrongLetterDisplay: " + wrongLetterDisplay);
         }
-        document.getElementById("wrong-guesses").innerHTML = wrongLetterDisplay;
-        console.log("Wrong letter display: " + wrongLetterDisplay);
     }
+    document.getElementById("wrong-guesses").innerHTML = wrongLetterDisplay;
+    console.log("Wrong letter display: " + wrongLetterDisplay);
 }
 
 initializeSettings();
@@ -115,52 +118,59 @@ initializeSettings();
 document.onkeyup = function (event) {
     var keyCommand = event.key;
     console.log("keyCommand: " + keyCommand);
-    isCorrectLetter = false;
+    keyCommand = keyCommand.toLowerCase();
     if (keyCommand in alphabet) {
-        if (alphabet[keyCommand] === "unselected") {
-            for (var i = 0; i < puzzleAnswer.length; i++) { // update "word-blanks" and "wrong-guesses" IDs in index.html
-                if (alphabet[keyCommand.toLowerCase()] === "correct") {
-                    answerSoFar += puzzleAnswer.charAt(i).toUpperCase() + " ";
-                } else if (puzzleAnswer.charAt(i).toLowerCase() == keyCommand) { // found match in puzzleAnswer
-                    answerSoFar += keyCommand.toUpperCase() + " ";
-                    correctLetters++;
-                    numberOfGuesses--;
-                    displayNumberOfGuesses(numberOfGuesses);
-                    console.log("answerSoFar: " + answerSoFar);
-                    if (i === puzzleAnswer.length - 1) {
-                        alphabet[keyCommand] = "correct";
-                    }
-                    if (puzzleAnswer.length === correctLetters) {
-                        // if puzzle is solved, output congratulations message, increment wins variable
-                        // display all composer information, and play youtube video in background
-                        wins++;
-                        document.getElementById("win-counter").innerHTML = wins;
-                        document.getElementById("composer-name").innerHTML = composers[randomComposerIndex][1] + ", " + composers[randomComposerIndex][2]; // displays last name, comma, and first name
-                        document.getElementById("years-lived").innerHTML = composers[randomComposerIndex][3]; // composers[randomComposerIndex][3] is years lived
-                        document.getElementById("piece").innerHTML = composers[randomComposerIndex][4]; // composers[randomComposerIndex][4] is title of piece
-                        document.getElementById("performer").innerHTML = composers[randomComposerIndex][5]; //  composers[randomComposerIndex][5] is performer
-                        document.getElementById("video-player").innerHTML = composers[randomComposerIndex][6]; // composers[randomComposerIndex][6] is the iframe tag to the video
-                        console.log("Youtube iframe link: " + composers[randomComposerIndex][6]);
-                        alert("You win!");
-                        initializeSettings();
-                    }
-                } else {
-                    answerSoFar += "_ ";
-                }
+        correctTimesInAnswer = 0;
+        answerSoFar = ""; // reset answerSoFar
+        // count correct letters whenever a letter is pressed
+        for (var i = 0; i < puzzleAnswer.length; i++) { // update "word-blanks" and "wrong-guesses" IDs in index.html
+            if (alphabet[puzzleAnswer.charAt(i).toLowerCase()] === "correct" && puzzleAnswer.charAt(i).toLowerCase() === keyCommand) {
+                answerSoFar += puzzleAnswer.charAt(i).toUpperCase() + "  ";
+                correctTimesInAnswer++;
+            } else if (puzzleAnswer.charAt(i).toLowerCase() === keyCommand && alphabet[keyCommand] === "unselected") { // found match in puzzleAnswer
+                answerSoFar += puzzleAnswer.charAt(i).toUpperCase() + " ";
+                correctTimesInAnswer++;
+                alphabet[keyCommand] = "correct";
+                numberOfGuesses--; // only decrement numberOfGuesses the first time a letter is correct
+                displayNumberOfGuesses(numberOfGuesses);
+            } else if (alphabet[puzzleAnswer.charAt(i).toLowerCase()] === "correct") {
+                answerSoFar += puzzleAnswer.charAt(i).toUpperCase() + "  ";
+            } else {
+                answerSoFar += "_ ";
             }
+        }
+        correctLetters += correctTimesInAnswer;
+        console.log("Correct letters: " + correctLetters);
+        displayAnswerSoFar(answerSoFar);
+        if (puzzleAnswer.length === correctLetters) {
+            // if puzzle is solved, output congratulations message, increment wins variable
+            // display all composer information, and play youtube video in background
+            wins++;
             displayAnswerSoFar(answerSoFar);
-        } else if (alphabet[keyCommand] === "unselected") { //answer is wrong for the first time
+            document.getElementById("win-counter").innerHTML = wins;
+            document.getElementById("composer-name").innerHTML = composers[randomComposerIndex][1] + ", " + composers[randomComposerIndex][2]; // displays last name and first name
+            document.getElementById("years-lived").innerHTML = composers[randomComposerIndex][3]; // composers[randomComposerIndex][3] is years lived
+            document.getElementById("piece").innerHTML = composers[randomComposerIndex][4]; // composers[randomComposerIndex][4] is title of piece
+            document.getElementById("performer").innerHTML = composers[randomComposerIndex][5]; //  composers[randomComposerIndex][5] is performer
+            document.getElementById("video-player").innerHTML = composers[randomComposerIndex][6]; // composers[randomComposerIndex][6] is the iframe tag to the video
+            console.log("Youtube iframe link: " + composers[randomComposerIndex][6]);
+            alert("You won! The answer is " + composers[randomComposerIndex][1] + "!");
+            initializeSettings();
+        }
+        if (alphabet[keyCommand] === "unselected" && correctTimesInAnswer === 0) { // alphabet[keyCommand] which is still unselected must be wrong
             alphabet[keyCommand] = "wrong";
             wrongLetters.push(keyCommand.toUpperCase());
             displayWrongLetters(wrongLetters);
             numberOfGuesses--;
             displayNumberOfGuesses(numberOfGuesses);
-            if (numberOfGuesses === 0) {
-                losses++;
-                document.getElementById("loss-counter").innerHTML = losses;
-                alert("Sorry, you ran out of guesses.");
-                initializeSettings();
-            }
+        }
+        if (numberOfGuesses === 0) { // test if game is over
+            losses++;
+            displayAnswerSoFar();
+            document.getElementById("loss-counter").innerHTML = losses;
+            alert("You ran out of guesses. The answer is " + composers[randomComposerIndex][1] + ".");
+            initializeSettings();
         }
     }
 }
+
