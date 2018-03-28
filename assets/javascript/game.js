bcccc2a8eb29fbb46294816f3c258df96baca7ee
Jan Balangue <jantonio.balangue@gmail.com>
2018-03-28 14:02:30 -0700

    Refactored game.js and index.html to use JSON objects and jQuery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,102 @@
-// I know the number at element 0 of each array is worthless but the program will break if I remove it.
+
 var composers = [
-    ["goodvideo", "Rachmaninoff", "Sergei", "1873-1943", "Piano Concerto No. 2 Op. 18, 2nd Movement", "Yuja Wang - Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/qQCH0QPoCfs?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=4" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Schubert", "Franz", "1797-1828", "Impromptu Op. 90 No. 1", "Alfred Brendel - Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/zqqC0zbDkn4?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=12" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Beethoven", "Ludwig Van", "1770-1827", "Sonata Pathetique, 2nd Movement", "Alfred Brendel - Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/HrbZDgsZUBw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Holst", "Gustav", "1874-1934", "The Planets I. Mars, Bringer of War", "Royal Philharmonic Orchestra: Sir Charles Groves - Conductor", '<iframe width="560" height="315" src="https://www.youtube.com/embed/KUTByW8NElo?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Mozart", "Wolfgang Amadeus", "1756-1791", "Piano Sonata No. 11 in A, First Movement", "Mitsuko Uchida - Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/qbn692EKQLw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=2" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Chopin", "Frederic", "1810-1849", "Prelude No. 4 in E Minor", "Martha Argerich - Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/Tovh6JjaQ1A?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Satie", "Erik", "1866-1925", "Gymnopedie No. 1", "Lars Roos, Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/rIjWutnXZz8?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=9" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Barber", "Samuel", "1910-1981", "Adagio for Strings, Op. 11", "Atlanta Symphony Orchestra: Yoel Levi - Conductor", '<iframe width="560" height="315" src="https://www.youtube.com/embed/xDEvJ9vErJw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Bach", "Johann Sebastian", "1685-1750", "Cello Suite No. 1 - Prelude", "Yo-Yo Ma - Cello", '<iframe width="560" height="315" src="https://www.youtube.com/embed/PCicM6i59_I?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=4" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Debussy", "Claude", "1862-1918", "Reflets dans l'eau", "Jean-Efflam Bavouzet - Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/UAEvlfWB7V4?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=10" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Haydn", "Franz Joseph", "1732-1809", "Piano Sonata no. 59 in E flat major", "Alfred Brendel - Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/6WSGG__mG5g?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Mendelssohn", "Felix", "1809-1847", "Songs Without Words, No. 12 in F Sharp Minor, Op. 30, No. 6", "Christopher Guzman - Piano", '<iframe width="560" height="315" src="https://www.youtube.com/embed/usPP8BoCkaw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=5" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'],
-    ["goodvideo", "Massenet", "Jules", "1842-1912", "Meditation from Thais", "Japan Philharmonic Orchestra: Harada Koichiro - Conductor, Kamio Mayuko - Violin",'<iframe width="560" height="315" src="https://www.youtube.com/embed/G555rLsczMY?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=6" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>']
+    {
+        lastName: "Rachmaninoff",
+        firstName: "Sergei",
+        yearsLived: "1873-1943",
+        piece: "Piano Concerto No. 2 Op. 18, 2nd Movement",
+        performer: "Yuja Wang - Piano",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/qQCH0QPoCfs?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=4" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+
+    {
+        lastName: "Beethoven",
+        firstName: "Ludwig Van",
+        yearsLived: "1770-1827",
+        piece: "Sonata Pathetique, 2nd Movement",
+        performer: "Alfred Brendel - Piano",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/HrbZDgsZUBw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        firstName: "Holst",
+        lastName: "Gustav",
+        yearsLived: "1874-1934",
+        piece: "The Planets I. Mars, Bringer of War",
+        performer: "Royal Philharmonic Orchestra: Sir Charles Groves - Conductor",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/KUTByW8NElo?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        lastName: "Mozart",
+        firstName: "Wolfgang Amadeus",
+        yearsLived: "1756-1791",
+        piece: "Piano Sonata No. 11 in A, First Movement",
+        performer: "Mitsuko Uchida - Piano",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/qbn692EKQLw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=2" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        lastName: "Chopin",
+        firstName: "Frederic",
+        yearsLived: "1810-1849",
+        piece: "Prelude No. 4 in E Minor",
+        performer: "Martha Argerich - Piano",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/Tovh6JjaQ1A?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        lastName: "Satie",
+        firstName: "Erik",
+        yearsLived: "1866-1925",
+        piece: "Gymnopedie No. 1",
+        performer: "Lars Roos, Piano",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/rIjWutnXZz8?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=9" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        lastName: "Barber",
+        firstName: "Samuel",
+        yearsLived: "1910-1981",
+        piece: "Adagio for Strings, Op. 11",
+        performer: "Atlanta Symphony Orchestra: Yoel Levi - Conductor",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/xDEvJ9vErJw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        lastName: "Bach",
+        firstName: "Johann Sebastian",
+        yearsLived: "1685-1750",
+        piece: "Cello Suite No. 1 - Prelude",
+        performer: "Yo-Yo Ma - Cello",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/PCicM6i59_I?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=4" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        lastName: "Debussy",
+        firstName: "Claude",
+        yearsLived: "1862-1918",
+        piece: "Reflets dans l'eau",
+        performer: "Jean-Efflam Bavouzet - Piano",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UAEvlfWB7V4?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=10" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        lastName: "Haydn",
+        firstName: "Franz Joseph",
+        yearsLived: "1732-1809",
+        piece: "Piano Sonata no. 59 in E flat major",
+        performer: "Alfred Brendel - Piano",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/6WSGG__mG5g?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        lastName: "Mendelssohn",
+        firstName: "Felix",
+        yearsLived: "1809-1847",
+        piece: "Songs Without Words, No. 12 in F Sharp Minor, Op. 30, No. 6",
+        performer: "Christopher Guzman - Piano",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/usPP8BoCkaw?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=5" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    },
+    {
+        lastName: "Massenet",
+        firstName: "Jules",
+        yearsLived: "1842-1912",
+        piece: "Meditation from Thais",
+        performer: "Japan Philharmonic Orchestra: Harada Koichiro - Conductor, Kamio Mayuko - Violin",
+        youTubeLink: '<iframe width="560" height="315" src="https://www.youtube.com/embed/G555rLsczMY?autoplay=1&rel=0&amp;controls=0&amp;showinfo=0&amp;start=6" frameborder="0" allow="encrypted-media" allowfullscreen></iframe>'
+    }
 ];
 
 // play game at least once
@@ -68,7 +152,7 @@ function initializeSettings() {
 
     randomComposerIndex = Math.floor(Math.random() * composers.length);
     // assign random composer as the hangman answer
-    puzzleAnswer = composers[randomComposerIndex][1].toUpperCase(); // makes answer uppercase; index [][1] is the composer's last name
+    puzzleAnswer = composers[randomComposerIndex].lastName.toUpperCase(); // makes answer uppercase; index [][1] is the composer's last name
     // console.log("randomComposerIndex: " + randomComposerIndex);
     // console.log("puzzleAnswer: " + puzzleAnswer);
 
@@ -76,8 +160,8 @@ function initializeSettings() {
     for (var letter in alphabet) {
         alphabet[letter] = "unselected";
     }
-    document.getElementById("win-counter").innerHTML = wins;
-    document.getElementById("loss-counter").innerHTML = losses;
+    $("#win-counter").html(wins);
+    $("#loss-counter").html(losses);
 
 
     answerSoFar = "";
@@ -94,12 +178,12 @@ function initializeSettings() {
 }
 
 function displayNumberOfGuesses(number) {
-    document.getElementById("guesses-left").innerHTML = number;
+    $("#guesses-left").html(number);
     // console.log("numberOfGuesses: " + number);
 }
 
 function displayAnswerSoFar(answerSoFar) {
-    document.getElementById("word-blanks").innerHTML = answerSoFar;
+    $("#word-blanks").html(answerSoFar);
     // console.log("answerSoFar: " + answerSoFar);
 }
 
@@ -112,7 +196,7 @@ function displayWrongLetters(wrongLetters) {
             // console.log("wrongLetterDisplay: " + wrongLetterDisplay);
         }
     }
-    document.getElementById("wrong-guesses").innerHTML = wrongLetterDisplay;
+    $("#wrong-guesses").html(wrongLetterDisplay);
     // console.log("Wrong letter display: " + wrongLetterDisplay);
 }
 
@@ -151,14 +235,13 @@ document.onkeyup = function (event) {
             // display all composer information, and play youtube video in background
             wins++;
             displayAnswerSoFar(answerSoFar);
-            document.getElementById("win-counter").innerHTML = wins;
-            document.getElementById("composer-name").innerHTML = composers[randomComposerIndex][1] + ", " + composers[randomComposerIndex][2]; // displays last name and first name
-            document.getElementById("years-lived").innerHTML = composers[randomComposerIndex][3]; // composers[randomComposerIndex][3] is years lived
-            document.getElementById("piece").innerHTML = composers[randomComposerIndex][4]; // composers[randomComposerIndex][4] is title of piece
-            document.getElementById("performer").innerHTML = composers[randomComposerIndex][5]; //  composers[randomComposerIndex][5] is performer
-            document.getElementById("video-player").innerHTML = composers[randomComposerIndex][6]; // composers[randomComposerIndex][6] is the iframe tag to the video
-            // console.log("Youtube iframe link: " + composers[randomComposerIndex][6]);
-            alert("You won! The answer is " + composers[randomComposerIndex][1] + "!");
+            $("#win-counter").html(wins);
+            $("#composer-name").html(composers[randomComposerIndex].lastName + ", " + composers[randomComposerIndex].firstName); // displays last name and first name
+            $("#years-lived").html(composers[randomComposerIndex].yearsLived); // composers[randomComposerIndex][3] is years lived
+            $("#piece").html(composers[randomComposerIndex].piece); // composers[randomComposerIndex][4] is title of piece
+            $("#performer").html(composers[randomComposerIndex].performer); //  composers[randomComposerIndex][5] is performer
+            $("#video-player").html(composers[randomComposerIndex].youTubeLink); // composers[randomComposerIndex][6] is the iframe tag to the video
+            alert("You won! The answer is " + composers[randomComposerIndex].lastName + "!");
             initializeSettings();
         }
         if (alphabet[keyCommand] === "unselected" && correctTimesInAnswer === 0) { // alphabet[keyCommand] which is still unselected must be wrong
@@ -171,8 +254,8 @@ document.onkeyup = function (event) {
         if (numberOfGuesses === 0) { // test if game is over
             losses++;
             displayAnswerSoFar();
-            document.getElementById("loss-counter").innerHTML = losses;
-            alert("You ran out of guesses. The answer is " + composers[randomComposerIndex][1] + ".");
+            $("#loss-counter").html(losses);
+            alert("You ran out of guesses. The answer is " + composers[randomComposerIndex].lastName + ".");
             initializeSettings();
         }
     }
